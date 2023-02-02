bc6620301dd514ff2caf8d237701103f2cfd8a24
Filip Sjöberg <filip.sjoberg@hotmail.com>
2023-02-02 16:30:00 +0100

    Push existing project to GitLab

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl uppercase'>Välkommen till Södrabloggen</h1>
+            <div>this is content on that page</div>
+        </div>
+    );
+};
+
+export default HomePage;
